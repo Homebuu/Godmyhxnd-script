aaeee3646a0e2c9fa7f244b9d242bfa45ea6900d
Chaipat <87646794+Homebuu@users.noreply.github.com>
2026-09-06 17:54:42 +0700

    Update god.lua

--- god.lua
+++ god.lua
@@ -1,13 +1,1 @@
-local Games = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/Homebuu/Godmyhxnd-script/refs/heads/main/Godmyhxnd.lua"
-))()
-
-local Script = Games[game.GameId]
-
-if Script then
-    if Script:match("^https?://") then
-        loadstring(game:HttpGet(Script))()
-    else
-        loadstring(Script)()
-    end
-end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Homebuu/Godmyhxnd-script/refs/heads/main/Godmyhxnd.lua'))()
